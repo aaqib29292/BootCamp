e0901f932779bb355233dadf85872c308887b868
Aaqib Parvez <aaqib29292@gmail.com>
2016-02-21 13:04:23 +0530

    add task resource

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "signup" => "home#signup", as: :signup
   get "contact" => "home#contact", as: :contact
 
+
+  resources :tasks, except: [:show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218064355) do
+ActiveRecord::Schema.define(version: 20160221063311) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "status",      default: 0
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
